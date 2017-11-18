d9964661d1120441f6e62c7fa145a1f318cdaafd
Tiago Maluta <tiago@fundacaolemann.org.br>
2017-11-17 23:44:28 -0200

    edit + button
    Signed-off-by: Tiago Maluta <tiago@fundacaolemann.org.br>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import PQEAIbutton from './components/pqeai_button'
+import PQEAIinput from './components/pqeai_input'
 import './App.css';
 
 class App extends Component {
@@ -8,10 +10,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">PARA QUE EU APRENDO ISSO?</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <PQEAIinput />
+          <PQEAIbutton />
         </p>
       </div>
     );
